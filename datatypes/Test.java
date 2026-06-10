fbba651707702b4aadcda49c6808ada9742d3d2b
Lakshay Sharma <lakshaysasharma@gmail.com>
2026-06-10 14:56:04 +0530

    added data types

---
+++ datatypes/Test.java
@@ -0,0 +1,39 @@
+package datatypes;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int a = 1;
+        byte b = 1;
+        short c = 1;
+        long d = 1;
+
+        // System.out.println("Byte min value: " + Byte.MIN_VALUE );
+        // System.out.println("Byte max value: " + Byte.MAX_VALUE );
+
+        // System.out.println("Short min value: " + Short.MIN_VALUE );
+        // System.out.println("Short max value: " + Short.MAX_VALUE );
+
+        // System.out.println("Int max value: " + Integer.MAX_VALUE );
+        // System.out.println("Int max value: " + Integer.MAX_VALUE );
+
+        // System.out.println("Long min value: " + Long.MIN_VALUE );
+        // System.out.println("Long max value: " + Long.MAX_VALUE );
+
+
+        char m = 'm';
+        System.out.println((int)m);
+
+        System.out.println("Char min value: " + (int) Character.MIN_VALUE );
+        System.out.println("Char max value: " + (int) Character.MAX_VALUE );
+
+        // System.out.println(c + d);
+
+
+
+
+
+
+
+    }
+}
