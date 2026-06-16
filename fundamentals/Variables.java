524793779593d4ac8ba992f3f9bc4c095f703c8e
Lakshay Sharma <lakshaysasharma@gmail.com>
2026-06-16 18:23:14 +0530

    created variables

---
+++ fundamentals/Variables.java
@@ -0,0 +1,32 @@
+package fundamentals;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 25;
+
+        System.out.println(age);
+
+        int count = 100;  // whole numbers
+        double price = 99.99;  // decimal numbers
+        boolean isOpen = true;  // true or false
+        char grade = 'A';  // single character(in single quotes)
+
+        System.out.println(count);
+        System.out.println(price);
+        System.out.println(isOpen);
+        System.out.println(grade);
+
+
+        long bigNum = 9876543213214L;  // long whole numbers
+        short small = 32000;  // small whole numbers
+        byte tiny = 127;  // very small whole numbers
+        float temp = 99.9f;  // small decimal numbers; double is preferred
+
+        System.out.println(bigNum);
+        System.out.println(small);
+        System.out.println(tiny);
+        System.out.println(temp);
+
+
+    }
+}
