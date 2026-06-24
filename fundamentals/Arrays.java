a0e523e0e0241a5ea2e41d38d242c9fa675bf341
Lakshay Sharma <lakshaysasharma@gmail.com>
2026-06-24 16:51:45 +0530

    method

--- fundamentals/Arrays.java
+++ fundamentals/Arrays.java
@@ -6,21 +6,21 @@ public class Arrays {
         
         int[] scores = {91, 75, 65, 85, 63};
 
-        // System.out.println(scores);  // gives a giberish code
-        // System.out.println(scores[1]);
-        // System.out.println(scores[2]);
+        System.out.println(scores);  // gives a giberish code
+        System.out.println(scores[1]);
+        System.out.println(scores[2]);
 
         scores[1] = 100;
-        // System.out.println(scores[1]);
+        System.out.println(scores[1]);
 
-        // System.out.println(scores.length);  // 5
+        System.out.println(scores.length);  // 5
 
         String str = "Lakshay";
-        // System.out.println(str.length());  // 7
+        System.out.println(str.length());  // 7
 
         int[] marks = {50, 55, 65, 85, 90};
         for(int i = 0; i < marks.length; i++) {
-            // System.out.println(marks[i]);
+            System.out.println(marks[i]);
         }
 
 
@@ -30,7 +30,7 @@ public class Arrays {
         for(int i = 0; i < nums.length; i++) {
             total += nums[i];
         }
-        // System.out.println("Total: " + total);
+        System.out.println("Total: " + total);
 
 
         // The enhanced for loop (for-each)
@@ -43,27 +43,15 @@ public class Arrays {
         System.out.println(names[1]);
 
 
-
-
-
-
         // int[] marks = new int[5];
-        // int[] marks = {91, 92, 93, 94, 95};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        marks[0] = 45;
+        marks[1] = 55;
+        marks[2] = 65;
+        marks[3] = 75;
+        marks[4] = 85;
+        for(int mark : marks) {
+            System.out.println(mark);
+        }
 
 
     }
--- fundamentals/Methods.java
+++ fundamentals/Methods.java
@@ -1,5 +1,6 @@
 package fundamentals;
 
+
 public class Methods {
     public static void main(String[] args) {
         int result = sum(3, 5);
@@ -8,7 +9,18 @@ public class Methods {
         System.out.println(sum(10, 20));
 
         greet("Lakshay");
+
+        checkAge(-1);
+
+        printLine();
+
+        System.out.println(add(2, 3));
+        System.out.println(add(3.4, 4.5));
+        System.out.println(add(2, 4, 5));
+
+        System.out.println("Is 35 even? : " + isEven(35));
     }
+
 
     public static int sum(int a, int b) {
         return a + b;
@@ -16,6 +28,36 @@ public class Methods {
 
     public static void greet(String name) {
         System.out.println("Hello " + name + "!");
+    }
+
+    public static void checkAge(int age) {
+        if (age < 0) {
+            System.out.println("Invalid age");
+            return;
+        } 
+        System.out.println("Valid age");
+    }
+
+    public static void printLine() {
+        System.out.println("----------------------");
+    }
+
+
+    // Method overloading
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static double add(double a, double b) {
+        return a + b;
+    }
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+
+    // Is even?
+    public static boolean isEven(int number) {
+        return (number % 2 == 0);
     }
 
 
