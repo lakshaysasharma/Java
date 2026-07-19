355fd4a44f1846dc975075a45fc58e2a63785694
Lakshay Sharma <lakshaysasharma@gmail.com>
2026-07-19 10:48:29 +0530

    patterns

---
+++ object_oriented_programming/Car.java
@@ -0,0 +1,18 @@
+package object_oriented_programming;
+
+public class Car {
+    
+    String brand;
+    String color;
+    int speed;
+
+    void accelerate() {
+        speed += 10;
+        System.out.println(brand + " sped up to " + speed);
+    }
+
+    
+
+
+
+}
---
+++ patterns/Pattern_2.java
@@ -0,0 +1,23 @@
+package patterns;
+
+public class Pattern_2 {
+    public static void main(String[] args) {
+
+        int n = 4;
+        
+        for(int i = 0; i <= n; i++ ) {
+            for(int j = 0; j <= n; j++) {
+                if(i == 0 || j == 0 || i == n || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+        System.out.println();
+        }
+    }
+}
+
+
+
+
