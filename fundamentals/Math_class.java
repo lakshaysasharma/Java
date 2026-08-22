4549b2ac74b530f79789e1f6d20efc2d1e8fd154
Lakshay Sharma <lakshaysasharma@gmail.com>
2026-08-22 20:21:13 +0530

    Math class

---
+++ fundamentals/Math_class.java
@@ -0,0 +1,12 @@
+package fundamentals;
+
+public class Math_class {
+    
+    public static void main(String[] args) {
+        System.out.println(Math.max(5, 6));
+        System.out.println(Math.max(12, 15));
+
+        System.out.println((int)(Math.random()*100));
+    }
+}
+
